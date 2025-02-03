a81c522a6a7de06c66772acfae91a302cdf4847e
Joona <joona.kivela@student.lab.fi>
2025-02-03 16:56:37 +0200

    uibuild

--- Osa 3/puhelinluettelo/index.js
+++ Osa 3/puhelinluettelo/index.js
@@ -15,14 +15,14 @@ app.use(express.json())
 // Used for logging requests to console
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan((tokens, req, res) => {
-    return [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms',
-        tokens.body(req, res)
-    ].join(' ')
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res)
+  ].join(' ')
 }))
 
 // Allows server to accept requests from different origins (domains).
@@ -38,37 +38,37 @@ app.use(express.static('dist'))
 
 // Homepage
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!<h1/>')
+  response.send('<h1>Hello World!<h1/>')
 })
 
 // Get info
 app.get('/info', (request, response, next) => {
-    Person.countDocuments()
+  Person.countDocuments()
     .then(result => {
-    const responseMessage = `<h2>Phonebook has info for ${result} people</h2> \n ${new Date()}`
-    response.send(responseMessage)
+      const responseMessage = `<h2>Phonebook has info for ${result} people</h2> \n ${new Date()}`
+      response.send(responseMessage)
     })
     .catch(error => next(error))
-    
+
 })
 
 // Get persons
 app.get('/api/persons', (request, response) => {
-    //response.json(persons)
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  //response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 // Get 1 person
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
+  Person.findById(request.params.id)
     .then(person => {
-        if(person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
+      if(person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
     .catch(error => next(error))
 })
@@ -76,29 +76,29 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 // Add person
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const person =  new Person({
-        name: body.name,
-        number: body.number,
-    })
+  const person =  new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const {name, number} = request.body
+  const { name, number } = request.body
 
-    Person.findByIdAndUpdate(
-        request.params.id,
-        {name, number},
-         {new: true, runValidators: true, context: query}
-        )
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedPerson => {
-        response.json(updatedPerson)
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
@@ -106,16 +106,16 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 // Delete person
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 // Unknown endpoint
@@ -124,14 +124,14 @@ app.use(unknownEndpoint)
 
 // Defining errorHandler
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    
-    if(error.name === 'CastError') {
-        response.status(400).send({error: 'malformatted id'});
-    } else if (error.name === 'ValidationError')
-        return response.status(400).json({error: error.message})
+  console.log(error.message)
 
-    next(error)  
+  if(error.name === 'CastError') {
+    response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError')
+    return response.status(400).json({ error: error.message })
+
+  next(error)
 }
 
 // Use the errorhandler for errors
@@ -140,6 +140,6 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    
+  console.log(`Server running on port ${PORT}`)
+
 })
--- Osa 3/puhelinluettelo/models/person.js
+++ Osa 3/puhelinluettelo/models/person.js
@@ -4,23 +4,23 @@ const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 
-console.log("Connecting to ", url);
+console.log('Connecting to ', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB', error.message)
-    })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB', error.message)
+  })
 
 function validator(val) {
   if (val.length < 8) {
-    console.log("number too short");
+    console.log('number too short')
     return false
   }
-  let fields = val.split("-")
+  let fields = val.split('-')
   if (fields[0].length < 2 || fields[0].length > 3) {
-    console.log("no 2 or 3 numbers before '-'", fields[0].length);
+    console.log('no 2 or 3 numbers before \'-\'', fields[0].length)
     return false
   }
   return !isNaN(fields[0]) && !isNaN(fields[1])
@@ -41,11 +41,11 @@ const personSchema = new mongoose.Schema({
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
